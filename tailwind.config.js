3997181d9a0268d155d30a9d286b7facd216e515
Adebozz <ademolaadeigbe3@gmail.com>
2025-06-01 14:16:56 +0100

    `Update dependencies and remove unused files`
    `package-lock.json` and `package.json` updated with new dependencies and versions.
    `src/app/favicon.ico`, `src/app/globals.css`, `src/app/layout.tsx`, and `src/app/page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
