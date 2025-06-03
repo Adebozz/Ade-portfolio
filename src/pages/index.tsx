d2fe61ac8b894118a1bd0e6d02b38743f8ecbbde
Adebozz <ademolaadeigbe3@gmail.com>
2025-06-03 23:02:35 +0100

    `welcome`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
 // src/pages/index.tsx
 export default function Home() {
-  return <h1 className="text-2xl font-bold">Hello Portfolio!</h1>;
+  return <h1 className="text-2xl font-bold">Hello Portfolio! WELCOME</h1>;
 }
